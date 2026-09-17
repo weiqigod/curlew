c661087edf93cbfd527bca89336a9b698acdf86b
Peter Lindqvist <peter.lindqvist@norionbank.se>
2026-09-17 09:37:54 +0200

    fix(plugin): contain plugin process trees
    Refs: M30-006

---
+++ internal/plugin/process_posix.go
@@ -0,0 +1,57 @@
+//go:build !windows
+
+package plugin
+
+import (
+	"fmt"
+	"io"
+	"os/exec"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func spawnPlugin(path string, stderr io.Writer) (io.WriteCloser, io.ReadCloser, func(), error) {
+	cmd := exec.Command(path) //nolint:gosec // plugin path is explicitly configured
+	cmd.Stderr = stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("stdin pipe: %w", err)
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		_ = stdin.Close()
+		return nil, nil, nil, fmt.Errorf("stdout pipe: %w", err)
+	}
+	if err := cmd.Start(); err != nil {
+		_ = stdin.Close()
+		_ = stdout.Close()
+		return nil, nil, nil, fmt.Errorf("start: %w", err)
+	}
+
+	waited := make(chan struct{})
+	go func() {
+		_ = cmd.Wait()
+		close(waited)
+	}()
+	var once sync.Once
+	stop := func() {
+		once.Do(func() {
+			_ = stdin.Close()
+			select {
+			case <-waited:
+			case <-time.After(shutdownTimeout):
+				_ = syscall.Kill(-cmd.Process.Pid, syscall.SIGTERM)
+				select {
+				case <-waited:
+				case <-time.After(shutdownTimeout):
+					_ = syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+					<-waited
+				}
+			}
+			_ = syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+		})
+	}
+	return stdin, stdout, stop, nil
+}
